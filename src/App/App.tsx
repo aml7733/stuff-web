cb9991aa62368c2d88f00a3ad93e3610c0a011fb
aml7733 <lucia.adam@gmail.com>
2020-10-26 21:21:57 -0400

    add configs and packages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,10 @@
 import React, {ReactElement} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App(): ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
